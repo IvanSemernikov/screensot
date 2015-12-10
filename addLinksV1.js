bd4c9d6c9cf008569b1ce63144210d16d796366d
Ivan Semernikov <i.semernikov@aimprosoft.com>
2015-12-10 16:01:06 +0200

    dgfb

--- addLinksV1.js
+++ addLinksV1.js
@@ -3,22 +3,18 @@ document.addEventListener('DOMContentLoaded', function () {
 }, false);
 
 var controller = (function () {
-
     $this = {
         startTag: '@link@',
         endTag: '.png',
-
         addLinks: function () {
             var elemList = $this.getElements("#main-panel-content td.pane div.failure-summary pre");
             for (i = 0; i < elemList.length; i++) {
                 $this.addLink(elemList[i], $this.getLink);
             }
         },
-
         getElements: function (selector) {
             var result = [],
                 elemList = document.querySelectorAll(selector);
-
             for (i = 0; i < elemList.length; i++) {
                 if ($this.hasLinkSrc(elemList[i])) {
                     result.push(elemList[i]);
@@ -26,28 +22,24 @@ var controller = (function () {
             }
             return result;
         },
-
         hasLinkSrc: function (element) {
             return element.innerHTML.indexOf($this.startTag) > 0
         },
-
         addLink: function (element, getter) {
-
             var text = element.innerHTML;
+            
             var start = text.indexOf($this.startTag),
-                end = text.indexOf($this.endTag) + 4;
-
+                end = text.substring(start).indexOf($this.endTag) + $this.endTag.length;
+           
             var before = text.substring(0, start),
                 after = text.substring(end);
 
             element.innerHTML = before + after + getter(text.substring(start, end));
         },
-
         getLink: function (src) {
-            return ' <a target="_blank" href="' + src.substring(6, src.length) + '">Screenshot</a>';
+            return ' <a target="_blank" href="' + src.substring($this.startTag.length, src.length) + '">Screenshot</a>';
         }
     };
-
     return {
         init: function () {
             document.body.addEventListener('click', function (e) {
