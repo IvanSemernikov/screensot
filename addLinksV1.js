1642c16c6928958641ff334809adc4c8cd741304
Ivan Semernikov <i.semernikov@aimprosoft.com>
2015-12-10 15:49:53 +0200

    dfbdf

--- addLinksV1.js
+++ addLinksV1.js
@@ -35,7 +35,7 @@ var controller = (function () {
 
             var text = element.innerHTML;
             var start = text.indexOf($this.startTag),
-                end = text.indexOf($this.endTag + 4);
+                end = text.indexOf($this.endTag) + 4;
 
             var before = text.substring(0, start),
                 after = text.substring(end);
